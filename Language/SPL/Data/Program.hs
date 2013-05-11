e4690de97e22b07ed1971f58a93d1e6981c41bb0
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2013-05-11 15:35:54 +0200

    moved pretty printer to Program.hs

--- Language/SPL/Data/Program.hs
+++ Language/SPL/Data/Program.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, OverlappingInstances #-}
 module Language.SPL.Data.Program where
 
 import Language.SPL.Data.Position
+
+import Language.SPL.Printer
+
+
+-- Types -----------------------------------------------------------------------
 
 type Program    = Constructs
 type Constructs = [Construct]
@@ -60,3 +66,88 @@ data UnaryOperator  = Not | Neg
 
 type Arrity      = Int
 type Signature   = [Type]
+
+
+-- Pretty Printer --------------------------------------------------------------
+
+instance Pretty Program where
+  pretty = sep . map pretty
+
+instance Pretty Construct where
+  pretty (Declaration t n e)    = pretty t <+> pretty n <+>
+                                  align (equals <+> pretty e) <> semi
+  pretty (Definition t n p d b) = empty <$>
+                                  pretty t <+> pretty n <+> pretty p <+>
+                                  block (pretty d <$> pretty b)
+
+instance Pretty Type where
+  pretty VOID       = annotation "Void"
+  pretty INT        = annotation "Int"
+  pretty BOOL       = annotation "Bool"
+  pretty (PAIR s t) = parensized [s,t]
+  pretty (LIST t)   = brackets (pretty t)
+  pretty (POLY s)   = annotation s
+
+instance Pretty Name where
+  pretty Print    = identifier "print"
+  pretty IsEmpty  = identifier "isEmpty"
+  pretty Head     = identifier "head"
+  pretty Tail     = identifier "tail"
+  pretty Fst      = identifier "fst"
+  pretty Snd      = identifier "snd"
+  pretty Main     = identifier "main"
+  pretty (Name s) = identifier s
+
+instance Pretty Parameters where
+  pretty = parensized
+
+instance Pretty Parameter where
+  pretty (Parameter t n) = pretty t <+> pretty n
+
+instance Pretty Arguments where
+  pretty = parensized
+
+instance Pretty Block where
+  pretty = vsep . map pretty
+
+instance Pretty Statement where
+  pretty (Assign n e)  = pretty n <+> equals <+> pretty e <> semi
+  pretty (Execute n a) = pretty n <> pretty a <> semi
+  pretty (Return m)    = keyword "return" <+> pretty m <> semi
+  pretty (If c t e)    = keyword "if" <+> parens (pretty c) <+> 
+                         block t <+>
+                         keyword "else" <+>
+                         block e
+  pretty (While c l)   = keyword "while" <+> parens (pretty c) <+>
+                         block l
+
+instance Pretty Expression where
+  pretty (Value n)     = pretty n
+  pretty (Integer i)   = constant i
+  pretty (Boolean b)   = constant b
+  pretty (Nil)         = constant "[]"
+  pretty (Pair x y)    = parensized [x,y]
+  pretty (Call n as)   = pretty n <> pretty as
+  pretty (Infix o l r) = parens . align $ pretty l <+> pretty o </> pretty r
+  pretty (Prefix o e)  = parens $ pretty o <> pretty e
+
+instance Pretty BinaryOperator where
+  pretty Add  = operator '+'
+  pretty Sub  = operator '-'
+  pretty Mul  = operator '*'
+  pretty Div  = operator '/'
+  pretty Mod  = operator '%'
+  pretty Eq   = operator "=="
+  pretty Ne   = operator "!="
+  pretty Lt   = operator '<'
+  pretty Gt   = operator '>'
+  pretty Le   = operator "<="
+  pretty Ge   = operator ">="
+  pretty And  = operator "&&"
+  pretty Or   = operator "||"
+  pretty Cons = operator ':'
+
+instance Pretty UnaryOperator where
+  pretty Not = operator '!'
+  pretty Neg = operator '-'
+
